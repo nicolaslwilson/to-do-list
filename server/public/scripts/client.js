d5a2d581939b695a0a26b5138010f90b70dc3706
Nic Wilson <nicwilson@gmail.com>
2017-03-24 15:55:46 -0500

    Add toDoForm submit event

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
   console.log("jQuery loaded");
+  addEventListeners();
 });
+
+function addEventListeners() {
+  $('.addToDoForm').on('submit', addToDoSubmit);
+
+}
+
+function addToDoSubmit(event) {
+  event.preventDefault();
+  console.log($('.addToDoInput').val());
+
+}
