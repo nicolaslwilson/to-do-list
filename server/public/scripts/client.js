8ac5b839fca51ce01cc3d096c84392e850c73554
Nic Wilson <nicwilson@gmail.com>
2017-03-24 16:30:06 -0500

    Add client.js get request and append to do list to dom

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   console.log("jQuery loaded");
   addEventListeners();
+  refreshDOM();
 });
 
 function addEventListeners() {
@@ -19,7 +20,38 @@ function addToDoSubmit(event) {
       toDoItem: toDoItem
     },
     success: function (response) {
-      console.log(response);
+      refreshDOM();
     }
   });
+}
+
+function refreshDOM() {
+  $('#toDoList').empty();
+  getToDoData();
+}
+
+function getToDoData() {
+  $.ajax({
+    type: 'GET',
+    url: '/todo',
+    success: function (response) {
+      console.log(response);
+      appendToDoList(response);
+    }
+  });
+}
+
+
+function appendToDoList(toDoArray) {
+  for (var i = 0; i < toDoArray.length; i++) {
+    $('#toDoList').append('<li>');
+    var $el = $('#toDoList').children().last();
+    $el.append('<p>').children('p').text(toDoArray[i].text);
+    $el.append('<button class="Complete">Complete</button>');
+    $el.append('<button class="Delete">Delete</button>');
+  }
+}
+
+function populateToDoListItem (toDoObject) {
+
 }
